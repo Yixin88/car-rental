4fdd9df2f9ba2949c2349756d27f0d9360a86383
Yixin <yixin88@hotmail.co.uk>
2023-04-13 19:11:41 +0100

    setup router enviroment

---
+++ src/layout/RootLayout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function RootLayout() {
+  return (
+    <header>
+        <nav>
+          <div>
+            
+          </div>
+        </nav>
+    </header>
+  )
+}
