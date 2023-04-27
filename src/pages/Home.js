3e38dab6c2e76cff71f57129ec6790026e008752
Yixin <yixin88@hotmail.co.uk>
2023-04-27 15:01:38 +0100

    Added basicBanner component and styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import HeroBanner from '../components/Home_Page/HeroBanner'
 import Booking from '../components/Home_Page/Booking'
 import PlanTripBanner from '../components/Home_Page/PlanTripBanner'
 import Fleet from '../components/Home_Page/Fleet'
+import BasicBanner from '../components/Home_Page/BasicBanner'
 
 export default function Home({fleetArray}) {
 
@@ -12,6 +13,7 @@ export default function Home({fleetArray}) {
         <Booking />
         <PlanTripBanner />
         <Fleet />
+        <BasicBanner />
     </>
   )
 }
