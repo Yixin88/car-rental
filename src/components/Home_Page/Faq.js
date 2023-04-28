67a763ca5a9ab9b66dd0c5e697e7c20eecffb0d1
Yixin <yixin88@hotmail.co.uk>
2023-04-28 16:06:05 +0100

    moved questions into an object array, and added 2 extra questions and answers

--- src/components/Home_Page/Faq.js
+++ src/components/Home_Page/Faq.js
@@ -1,6 +1,25 @@
 import React from 'react'
 
 export default function Faq() {
+
+  const questions = [
+    {
+      question: "What is special about comparing rental car deals",
+      answer: "Comparing rental car deals is important as it helps find the best deal that fits your budget and requirements, ensuring you get the most value for your money. By comparing various options, you can find deals that offer lower prices, additional services, or better car models. You can find car rental deals by researching online and comparing prices from different rental companies.",
+      id: 1
+    },
+    {
+      question: "How do I find the car rental deals",
+      answer: "You can find car rental deals by researching online and comparing prices from different rental companies. Websites such as Expedia, Kayak, and Travelocity allow you to compare prices and view available rental options. It is also recommended to sign up for email newsletters and follow rental car companies on social media to be informed of any special deals or promotions.",
+      id: 2
+    },
+    {
+      question: "How do I find such low rental car prices",
+      answer: "Book in advance: Booking your rental car ahead of time can often result in lower prices. Compare prices from multiple companies: Use websites like Kayak, Expedia, or Travelocity to compare prices from multiple rental car companies. Look for discount codes and coupons: Search for discount codes and coupons that you can use to lower the rental price. Renting from an off-airport location can sometimes result in lower prices.",
+      id: 3
+    }
+  ]
+
   return (
     <section className='faq-section'>
       <div className='faq-section__container'>
@@ -11,14 +30,19 @@ export default function Faq() {
         </div>
 
         <div className='faq-section__container__questions__container'>
-          <div className='question-container'>
-            <div className='question-container__heading'>
-              <h4>1. What is special about comparing rental car deals?</h4>
-              <i className='fa-solid fa-angle-down'></i>
-            </div>
-            <p>Comparing rental car deals is important as it helps find the best deal that fits your budget and requirements, ensuring you get the most value for your money. By comparing various options, you can find deals that offer lower prices, additional services, or better car models. You can find car rental deals by researching online and comparing prices from different rental companies.</p>
-          </div>
+          {questions.map(question => {
+            return(
+                <div className='question-container'>
+                  <div className='question-container__heading'>
+                    <h4>{questions.indexOf(question)+1}. {question.question}</h4>
+                    <i className='fa-solid fa-angle-down'></i>
+                  </div>
+                  <p>{question.answer}</p>
+                </div>
+            )
+          })}
         </div>
+
       </div>
     </section>
   )
