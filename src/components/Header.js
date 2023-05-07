88f73ad3b5c043a2c6b9b58b83e7433858e52aea
Yixin <yixin88@hotmail.co.uk>
2023-05-07 16:36:36 +0100

    added hamburger menu and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import Logo from '../images/logo/logo.png'
 
 export default function Header() {
+
+  const [ activeMenu, setActiveMenu ] = useState(false);
+
   return (
     <header>
         <nav className='nav-bar'>
@@ -25,6 +28,20 @@ export default function Header() {
                 <Link className='nav-bar__buttons__signIn'>Sign In</Link>
                 <Link className='nav-bar__buttons__register'>Register</Link>
             </div>
+        </nav>
+
+        <i class="fa-solid fa-bars hamburger-btn" onClick={() => {setActiveMenu(true)}}></i>
+
+        <nav className={activeMenu ? 'hamburger-menu active' : 'hamburger-menu'}>
+            <i class="fa-solid fa-xmark close-btn" onClick={() => {setActiveMenu(false)}}></i>
+            <NavLink onClick={() => {setActiveMenu(false)}}>Home</NavLink>
+            <NavLink onClick={() => {setActiveMenu(false)}}>About</NavLink>
+            <NavLink onClick={() => {setActiveMenu(false)}}>Vehicle Models</NavLink>
+            <NavLink onClick={() => {setActiveMenu(false)}}>Testimonials</NavLink>
+            <NavLink onClick={() => {setActiveMenu(false)}}>Our Team</NavLink>
+            <NavLink onClick={() => {setActiveMenu(false)}}>Contact</NavLink>
+            <Link className='mobile-signIn-btn signIn-btn'>Sign In</Link>
+            <Link className='mobile-signIn-btn register-btn'>Register</Link>
         </nav>
     </header>
   )
