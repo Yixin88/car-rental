3adb84996219e203f179b1204870b6554ee9ebd6
Yixin <yixin88@hotmail.co.uk>
2023-05-11 21:42:29 +0100

    added template for about company in about page

---
+++ src/components/Home_Page/AboutCompany.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AboutCompany() {
+  return (
+    <section className='about-company-section'>
+      <div className='about-company-section__container'>
+
+      </div>
+    </section>
+  )
+}
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SubPageBanner from '../components/SubPageBanner'
 import { useLocation } from 'react-router-dom'
+import AboutCompany from '../components/Home_Page/AboutCompany';
 
 export default function About() {
   const url = useLocation().pathname;
   return (
     <>
       <SubPageBanner pageTitle={url}/>
+      <AboutCompany />
     </>
   )
 }
