e0f7d65ff277ed2758771c62374defdcce1d82ac
Yixin <yixin88@hotmail.co.uk>
2023-05-13 22:52:29 +0100

    fixed space for pathname

--- src/components/SubPageBanner.js
+++ src/components/SubPageBanner.js
@@ -12,7 +12,7 @@ export default function SubPageBanner({ pageTitle, additionalStartText, addition
       <div className='subPage-banner__container'>
         <div className='subPage-banner__container__heading'>
           <h3>{startText} {pageTitle.slice(1).charAt(0).toUpperCase() + pageTitle.slice(2)} {endText}</h3>
-          <h4>Home {pageTitle.charAt(0) + startText + " " + pageTitle.charAt(1).toUpperCase() + pageTitle.slice(2) + " " + endText}</h4>
+          <h4>Home {pageTitle.charAt(0) + " " +startText + " " + pageTitle.charAt(1).toUpperCase() + pageTitle.slice(2) + " " + endText}</h4>
         </div>
       </div>
     </section>
