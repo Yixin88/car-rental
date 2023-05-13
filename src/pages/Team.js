8171421fcb457ee457fd16bd1c078612e54c44ab
Yixin <yixin88@hotmail.co.uk>
2023-05-13 14:35:51 +0100

    added employee object and grid and styles

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import SubPageBanner from '../components/SubPageBanner'
+import { useLocation } from 'react-router-dom'
+import People from '../components/Home_Page/People';
 
 export default function Team() {
+  const url = useLocation().pathname;
+
   return (
-    <div>Team</div>
+    <>
+      <SubPageBanner pageTitle={url} additionalStartText='our'/>
+      <People />
+    </>
   )
 }
