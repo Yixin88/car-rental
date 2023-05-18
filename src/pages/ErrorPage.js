429c6a7bc54a7ccd666effe3ba6cbc00932fc5a9
Yixin <yixin88@hotmail.co.uk>
2023-05-18 15:25:46 +0100

    error page and styling added

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 export default function ErrorPage() {
   return (
-    <section className='errorPage-section'>
-      <div className='errorPage-section__container'>
-        
+    <section className='subPage-banner error'>
+      <div className='bg-overlay'></div>
+      <div className='subPage-banner__container'>
+        <div className='subPage-banner__container__heading error'>
+          <i class="fa-regular fa-face-frown"></i>
+          <h2>404 Error</h2>
+          <h3>Page Not Found!</h3>
+          <Link to='/'>Back To Home</Link>
+        </div>
       </div>
     </section>
   )
