5ed62825d6e277a9517252127c5d5307a7d8e31b
Yixin <yixin88@hotmail.co.uk>
2023-05-20 18:10:40 +0100

    fixed subpage banner to crash if no pagetitle prop is passed

--- src/components/SubPageBanner.js
+++ src/components/SubPageBanner.js
@@ -10,10 +10,10 @@ export default function SubPageBanner({ pageTitle, additionalStartText, addition
     <section className='subPage-banner'>
       <div className='bg-overlay'></div>
       <div className='subPage-banner__container'>
-        <div className='subPage-banner__container__heading'>
+        {pageTitle && <div className='subPage-banner__container__heading'>
           <h3>{startText} {pageTitle.slice(1).charAt(0).toUpperCase() + pageTitle.slice(2)} {endText}</h3>
           <h4>Home {pageTitle.charAt(0) + " " +startText + " " + pageTitle.charAt(1).toUpperCase() + pageTitle.slice(2) + " " + endText}</h4>
-        </div>
+        </div>}
       </div>
     </section>
   )
